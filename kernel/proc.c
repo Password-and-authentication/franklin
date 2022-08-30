1926f7577e5bdefaa28a781de975d623437c7adf
pomelleero@gmail.com <pomelleero@gmail.com>
2022-08-30 21:45:31 +0300

    proc.c

--- kernel/proc.c
+++ kernel/proc.c
@@ -5,17 +5,14 @@
 #include "franklin/proc.h"
 #include "asm/x86.h"
 
-
+#define NPROC 255
 
 struct proc *curproc;
-struct proc ptable[20];
-static int ptable_index;
+struct proc ptable[NPROC];
 
-static uint8_t swap = 1;
+
 struct proc* set_current_proc(struct proc *p) {
   wrmsr((uint64_t)p);
-  if (swap == 0)
-    swap = 1;
   swapgs();
   return p;
 }
@@ -23,52 +20,61 @@ struct proc* set_current_proc(struct proc *p) {
 struct proc* get_current_proc() {
 
   struct proc *p;
-  if (swap)
-    swapgs(); swap = 0;
 
+  swapgs();
   rdmsr(&p);
+  if (p == 0) // if p = 0, it means swapgs() swapped the wrong way, so we need to swap again
+    swapgs(); rdmsr(&p);
   
   return p;
 }
 
 void startproc(struct proc *p) {
-  stack *l;
+
   struct proc *current;
+  
   current = get_current_proc();
   curproc = current;
   curproc->state = RUNNING;
-  switc(&current->stack, p->stack);
+
+  // this useless stack needs to be "saved" somewhere
+  stack *discard;
+  switc(&discard, p->stack);
 }
 
 void allocproc(uintptr_t *entry) {
-
+  static uint32_t nextpid = 0;
   struct proc *p;
   
-  p = &ptable[ptable_index++];
-  
+  p = &ptable[nextpid];
+  p->pid = nextpid++;
+
+  // palloc(1) allocates 1 page in phys memory and returns a physical addr
   p->stack = (stack*)P2V((uintptr_t)palloc(1));
   p->stack->rip = (uintptr_t)entry;
+    
   p->state = RUNNABLE;
 }
-
-
 
 
 void scheduler() {
 
   struct proc *p, *prev;
-  static uint8_t i = 1;
+  static uint8_t i = 1; // note: this is static
 
+  // find next runnable process
   while ((p = &ptable[i++]) && p->state != RUNNABLE) {
-    if (i == 20)
+    if (i == NPROC)
       i = 0;
   }
   
   prev = get_current_proc();
   curproc = set_current_proc(p);
   curproc->state = RUNNING;
+
+  // save current stack on the previous process's struct
+  // and switch to new process
   switc(&prev->stack, curproc->stack); 
-  
 }
 
 
