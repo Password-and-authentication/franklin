20249af672fbe2211d553278e1ffba00d53c20ee
pomelleero@gmail.com <pomelleero@gmail.com>
2022-09-27 23:52:30 +0300

    .

--- include/franklin/fs/dirent.h
+++ include/franklin/fs/dirent.h
@@ -1,17 +1,20 @@
+#ifndef _DIRENT_
+#define _DIRENT_
 
+#include "std/string.h"
+#include <stdint.h>
 
+#define DT_UNKNOWN 0
+#define DT_FIFO 1
+#define DT_CHR 2
+#define DT_DIR 4
+#define DT_BLK 6
+#define DT_REG 8
+#define DT_LNK 10
+#define DT_SOCK 12
 
-#define DT_UNKNOWN 	0
-#define DT_FIFO 	1
-#define DT_CHR 		2
-#define DT_DIR 		4
-#define DT_BLK 		6
-#define DT_REG 		8
-#define DT_LNK 		10
-#define DT_SOCK 	12
-
-
-struct dirent {
+struct dirent
+{
   ino_t ino;
   int reclen;
   char type;
@@ -20,7 +23,9 @@ struct dirent {
 
 // get size of dirent
 static inline uint16_t
-get_reclen(struct dirent *d)
+get_reclen(struct dirent* d)
 {
   return ((char*)&d->name + strlen(d->name) + 1) - (char*)d;
 }
+
+#endif
